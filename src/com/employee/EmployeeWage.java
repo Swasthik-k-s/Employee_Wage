8952e807e06c89130b4e1a18609fcc531e4e4488
Swasthik <swasthik.ks22@gmail.com>
2021-08-24 13:12:06 +0530

    UC5 - Monthly Wage Calculated

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -5,26 +5,30 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int WORKING_DAYS = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empDailyWage;
-		int empHour;
-		int empstatus = (int) Math.floor(Math.random() * 10) % 3;
-		switch(empstatus) {
-		case IS_FULL_TIME:
-			System.out.println("Full Time Employee");
-			empHour = 8;
-			break;
-		case IS_PART_TIME:
-			System.out.println("Part Time Employee");
-			empHour = 4;
-			break;
-		default:
-			empHour = 0;
+		int empDailyWage =0;
+		int empHour = 0;
+		int empTotalWage = 0;
+		for (int i = 0; i < WORKING_DAYS; i++) {
+			int empstatus = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empstatus) {
+			case IS_FULL_TIME:
+				empHour = 8;
+				break;
+			case IS_PART_TIME:
+				empHour = 4;
+				break;
+			default:
+				empHour = 0;
+			}
+			empDailyWage = WAGE_PER_HOUR * empHour;
+			empTotalWage += empDailyWage; 
+			System.out.println("Employee Wage on Day " + (i + 1) + " = " + empDailyWage);
 		}
-		empDailyWage = WAGE_PER_HOUR * empHour;
-		System.out.println("Employee Daily Wage = " + empDailyWage);
+		System.out.println("Employee Total Monthly Wage = " + empTotalWage);
 	}
 
 }

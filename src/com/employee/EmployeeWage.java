1786a48ecdf98f0113bc6177a9643169ad841f38
Swasthik <swasthik.ks22@gmail.com>
2021-08-24 08:28:33 +0530

    UC1 - Attendance Check is Completed

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -2,8 +2,16 @@ package com.employee;
 
 public class EmployeeWage {
 
+	public static final int IS_PRESENT = 1;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		double empstatus = Math.floor(Math.random() * 10) % 2;
+		if(empstatus == IS_PRESENT) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }

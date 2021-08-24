1bef0d2f14256a2519705ff8d81e83c1030a4f72
Swasthik <swasthik.ks22@gmail.com>
2021-08-24 12:08:07 +0530

    UC2 - Daily Wage Calculated

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -2,16 +2,23 @@ package com.employee;
 
 public class EmployeeWage {
 
-	public static final int IS_PRESENT = 1;
+	public static final int IS_FULL_TIME = 1;
+	public static final int WAGE_PER_HOUR = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int empDailyWage;
+		int empHour;
 		double empstatus = Math.floor(Math.random() * 10) % 2;
-		if(empstatus == IS_PRESENT) {
-			System.out.println("Employee is Present");
+		if(empstatus == IS_FULL_TIME) {
+			System.out.println("Full Time Employee");
+			empHour = 8;
 		} else {
-			System.out.println("Employee is Absent");
+			System.out.println("Part Time Employee");
+			empHour = 0;
 		}
+		empDailyWage = WAGE_PER_HOUR * empHour;
+		System.out.println("Employee Daily Wage = " + empDailyWage);
 	}
 
 }

032be73fa7ffdb4a35cd5ca0075d919c42bf5aab
Swasthik <swasthik.ks22@gmail.com>
2021-08-24 12:13:00 +0530

    UC3 - Part Time Hour Updated

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -3,18 +3,21 @@ package com.employee;
 public class EmployeeWage {
 
 	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	public static final int WAGE_PER_HOUR = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int empDailyWage;
 		int empHour;
-		double empstatus = Math.floor(Math.random() * 10) % 2;
+		double empstatus = Math.floor(Math.random() * 10) % 3;
 		if(empstatus == IS_FULL_TIME) {
 			System.out.println("Full Time Employee");
 			empHour = 8;
-		} else {
+		} else if(empstatus == IS_PART_TIME) {
 			System.out.println("Part Time Employee");
+			empHour = 4;
+		} else {
 			empHour = 0;
 		}
 		empDailyWage = WAGE_PER_HOUR * empHour;

ad416bd86b378afc0b7c78dd8861d60b877c93a7
Swasthik <swasthik.ks22@gmail.com>
2021-09-05 18:41:24 +0530

    Ability to save the total wage for each company

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -3,10 +3,24 @@ package com.employee;
 public class Employee {
 	final static int IS_FULL_TIME = 1;
 	final static int IS_PART_TIME = 2;
+	String companyName;
+	private int wagePerHour;
+	int maxWorkingDay;
+	int maxWorkingHour;
+	int empTotalWage;
 
-	public static void calculateWage(String companyName, int wagePerHour,  int maxWorkingDay, int maxWorkingHour) {
+	//Constructor is used to get the values while creating a object
+	public Employee(String companyName, int wagePerHour, int maxWorkingDay, int maxWorkingHour) {
+		this.companyName = companyName;
+		this.wagePerHour = wagePerHour;
+		this.maxWorkingDay = maxWorkingDay;
+		this.maxWorkingHour = maxWorkingHour;
+	}
+
+	//empWageBuilder calculates the monthly wage of a company
+	public void empWageBuilder() {
 		
-		int empHour, empTotalHour, empWorkingDay, empTotalWage;
+		int empHour, empTotalHour, empWorkingDay;
 		empHour = 0;
 		empTotalHour = 0;
 		empWorkingDay = 0;
@@ -32,5 +46,10 @@ public class Employee {
 		empTotalWage = empTotalHour * wagePerHour;
 		System.out.println("Company : " + companyName);
 		System.out.println("Employee Total Monthly Wage = " + empTotalWage);
+	}
+
+	@Override
+	public String toString() {
+		return "Company : " + companyName + "\nEmployee Total Monthly Wage = " + empTotalWage;
 	}
 }
